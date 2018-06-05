abd601f52c9ec16c3b806422e6294e67d1fec2ee
gpwclark <gpwclark@gmail.com>
2018-06-05 23:08:49 +0000

    added test-case

--- src/test/java/com/uofantarctica/dsync/model/RolodexTest.java
+++ src/test/java/com/uofantarctica/dsync/model/RolodexTest.java
@@ -1,8 +1,6 @@
 package com.uofantarctica.dsync.model;
 
 import com.uofantarctica.dsync.DSyncReporting;
-import com.uofantarctica.dsync.model.Rolodex;
-import com.uofantarctica.dsync.model.SyncState;
 import net.named_data.jndn.Interest;
 import net.named_data.jndn.Name;
 import org.junit.jupiter.api.AfterEach;
@@ -12,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class RolodexTest {
 	Rolodex rolodex;
@@ -34,9 +33,25 @@ public class RolodexTest {
 		.append(Long.toString(3l))
 		.append(Long.toString(defaultSeq));
 
+	Name name3a = new Name(dataPrefix)
+		.append("3" + uuid)
+		.append(Long.toString(4l))
+		.append(Long.toString(defaultSeq));
+
 	Interest interest1;
 	Interest interest2;
 	Interest interest3;
+	Interest interest3a;
+
+	public Rolodex makeNewRolodexWithSlightlyDifferentSeededData() {
+		SyncState s1 = new SyncState(interest1);
+		SyncState s2 = new SyncState(interest2);
+		SyncState s3 = new SyncState(interest3a);
+		Rolodex anotherRolodex = new Rolodex(s1, dataPrefix, new DSyncReporting("name", "id"));
+		anotherRolodex.add(s2);
+		anotherRolodex.add(s3);
+		return anotherRolodex;
+	}
 
 	public Rolodex makeNewRolodexWithSeededData() {
 		SyncState s1 = new SyncState(interest1);
@@ -48,7 +63,7 @@ public class RolodexTest {
 		return aRolodex;
 	}
 
-	public Rolodex makeNewRolodexWithDifferentlyOdderedSeededData() {
+	public Rolodex makeNewRolodexWithDifferentlyOrderedSeededData() {
 		SyncState s1 = new SyncState(interest1);
 		SyncState s2 = new SyncState(interest2);
 		SyncState s3 = new SyncState(interest3);
@@ -63,8 +78,9 @@ public class RolodexTest {
 		interest1 = new Interest(name1);
 		interest2 = new Interest(name2);
 		interest3 = new Interest(name3);
+		interest3a = new Interest(name3a);
 		rolodex = makeNewRolodexWithSeededData();
-		otherRolodex = makeNewRolodexWithDifferentlyOdderedSeededData();
+		otherRolodex = makeNewRolodexWithDifferentlyOrderedSeededData();
 	}
 
 	@AfterEach
@@ -94,5 +110,12 @@ public class RolodexTest {
 		for (int i = 0; i < rolodex.size(); i++) {
 			assertEquals(rolodex.get(i), matchingRolodex.get(i));
 		}
+	}
+
+	@Test
+	public void testSlightlyDifferentRolodexesHashDifferently() throws Exception {
+		Rolodex rolodex1 = makeNewRolodexWithSeededData();
+		Rolodex rolodex2 = makeNewRolodexWithSlightlyDifferentSeededData();
+		assertNotEquals(rolodex1, rolodex2);
 	}
 }
